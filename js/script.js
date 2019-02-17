efcab8a72ed925a8f59188406574cd14fe10f752
Piotr Przygoda <przygoda@gmail.com>
2019-02-17 12:24:52 +0100

    Kodilla example 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+'use strict';
+(function(){ 
+	
+	// Definujemy funkcję initMap w zakresie globalnym (czyli jako właściwość obiektu window).
+  	window.initMap = function() {
+		
+		// Zapisujemy w zmiennej obiekt zawierający współrzędne geograficzne.
+		var uluru = {lat: -25.363, lng: 131.044};
+		
+		// W zmiennej map zapisujemy nową instancję obiektu Map. 
+		var map = new google.maps.Map(document.getElementById('map'), {
+			// Podajemy opcje mapy, np. zoom i punkt wycentrowania mapy.
+			zoom: 4,
+			center: uluru
+		});
+		
+		// Definiujemy marker jako nową instancję obiektu Marker.
+		var marker = new google.maps.Marker({
+			// I podajemy opcje tego markera, np. na której mapie ma być dodany oraz jakie są jego współrzędne. 
+			position: uluru,
+			map: map
+		}); 
+	}	
+	 
+})();  
+
+/*
+UWAGA: Normalnie nie ma potrzeby wywołania funkcji initMap, tak jak robimy to poniżej. Musieliśmy jednak zmodyfikować sposób wczytywania skryptu Google Maps API ze względu na działanie CodePena. W Twoim kodzie nie powinno być tego wywołania. 
+*/
+
+initMap();
+
+
 var templateSlide = document.getElementById('templateCarousel').innerHTML;
 var carousel = document.querySelector('.main-carousel');
 
